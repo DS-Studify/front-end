938abdbe4125f0507ca65503b66eea73553c1254
Gyuri <ygreee0320@naver.com>
2025-07-06 01:20:38 +0900

    #15 feat: 통계 달력 component 추가

--- feature/statistics/src/main/java/com/ds/studify/feature/calendar/StatsScreen.kt
+++ feature/statistics/src/main/java/com/ds/studify/feature/calendar/StatsScreen.kt
@@ -1,16 +1,27 @@
 package com.ds.studify.feature.calendar
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ds.studify.core.designsystem.component.StudifyScaffoldWithLogo
+import com.ds.studify.core.designsystem.theme.StudifyColors
 import com.ds.studify.core.resources.StudifyDrawable
+import com.ds.studify.feature.calendar.component.StatsCalendar
+import java.time.YearMonth
 
 @Composable
 internal fun StatsRoute(
@@ -41,7 +52,27 @@ internal fun StatsRoute(
 internal fun StatsScreen(
     paddingValues: PaddingValues
 ) {
-
+    Column(
+        modifier = Modifier
+            .padding(
+                start = paddingValues.calculateStartPadding(LocalLayoutDirection.current),
+                end = paddingValues.calculateEndPadding(LocalLayoutDirection.current),
+                top = 0.dp,
+                bottom = paddingValues.calculateBottomPadding()
+            )
+            .fillMaxSize()
+            .background(color = StudifyColors.WHITE)
+    ) {
+        StatsCalendar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = paddingValues.calculateTopPadding()),
+            yearMonthState = YearMonth.now(),
+            studyTimeInMonth = emptyList(),
+            onMonthPickerClick = {},
+            onEvent = {}
+        )
+    }
 }
 
 @Preview
