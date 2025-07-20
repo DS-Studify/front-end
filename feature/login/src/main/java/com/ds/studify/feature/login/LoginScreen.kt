5f831bb3d18ed0b8bd2f63e9843017e85346f814
박유정 <p_1321@duksung.ac.kr>
2025-07-21 02:18:54 +0900

    #20 feat: login 모듈 추가

---
+++ feature/login/src/main/java/com/ds/studify/feature/login/LoginScreen.kt
@@ -0,0 +1,21 @@
+package com.ds.studify.feature.login
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun LoginScreen(
+    paddingValues: PaddingValues
+) {
+
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen(
+        paddingValues = PaddingValues(0.dp)
+    )
+}
