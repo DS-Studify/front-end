734247e89ba6db1b5ce44d0668dd4df7d74bdfda
Gyuri <ygreee0320@naver.com>
2025-07-30 23:15:47 +0900

    #30 design: 마이페이지 상단 padding 수정

--- feature/mypage/src/main/java/com/ds/studify/feature/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/ds/studify/feature/mypage/MyPageScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,10 +60,14 @@ internal fun MyPageScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues)
             .background(color = StudifyColors.WHITE)
+            .padding(
+                start = paddingValues.calculateStartPadding(LocalLayoutDirection.current),
+                end = paddingValues.calculateEndPadding(LocalLayoutDirection.current),
+                top = paddingValues.calculateTopPadding(),
+                bottom = paddingValues.calculateBottomPadding()
+            )
     ) {
-
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
