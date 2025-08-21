83b92e1c5de92162ebf549393137b6ca8b309ac2
Gyuri <ygreee0320@naver.com>
2025-08-21 13:42:28 +0900

    #68 design: DonutChart 모두 0%일 때 빈 차트 그리기

--- core/designsystem/src/main/java/com/ds/studify/core/designsystem/component/StudifyDonutChart.kt
+++ core/designsystem/src/main/java/com/ds/studify/core/designsystem/component/StudifyDonutChart.kt
@@ -142,6 +142,16 @@ private fun DonutChart(data: List<ChartSegment>) {
         val arcOffset = Offset(radiusOffset, radiusOffset)
         val totalTime = data.sumOf { it.time }.toFloat()
 
+        drawArc(
+            color = Color(0xFFF3F3F3),
+            startAngle = startAngle,
+            sweepAngle = 360f,
+            useCenter = false,
+            style = Stroke(width = strokeWidth, cap = StrokeCap.Butt),
+            size = arcSize,
+            topLeft = arcOffset
+        )
+
         data.forEach { segment ->
             val percentage = if (totalTime == 0f) 0f else (segment.time / totalTime)
             val sweepAngle = percentage * 360f
@@ -234,6 +244,18 @@ private fun DonutChartWithTwoStatePreview2() {
     StudifyDonutChartWithState(
         listOf(
             ChartSegment("바른 자세", StudifyColors.PK02, 24300),
+            ChartSegment("나쁜 자세", StudifyColors.G01, 0),
+        ),
+        "*공부 시간 중 자세의 비율"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DonutChartWithTwoStatePreview3() {
+    StudifyDonutChartWithState(
+        listOf(
+            ChartSegment("바른 자세", StudifyColors.PK02, 0),
             ChartSegment("나쁜 자세", StudifyColors.G01, 0),
         ),
         "*공부 시간 중 자세의 비율"
