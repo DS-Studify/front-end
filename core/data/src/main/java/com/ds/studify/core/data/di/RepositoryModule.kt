e849fb1ed35c0cf9c96b7e4c167d6e5f6a3952e6
Gyuri <ygreee0320@naver.com>
2025-08-21 13:59:55 +0900

    #70 refactor: UserDataModule 내용 공통 모듈로 이동

--- core/data/src/main/java/com/ds/studify/core/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/ds/studify/core/data/di/RepositoryModule.kt
@@ -4,10 +4,12 @@ import com.ds.studify.core.data.repository.AuthRepository
 import com.ds.studify.core.data.repository.StudyRecordRepository
 import com.ds.studify.core.data.repository.StudyRepository
 import com.ds.studify.core.data.repository.TokenRepository
+import com.ds.studify.core.data.repository.UserRepository
 import com.ds.studify.core.data.repository_impl.AuthRepositoryImpl
 import com.ds.studify.core.data.repository_impl.StudyRecordRepositoryImpl
 import com.ds.studify.core.data.repository_impl.StudyRepositoryImpl
 import com.ds.studify.core.data.repository_impl.TokenRepositoryImpl
+import com.ds.studify.core.data.repository_impl.UserRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -36,5 +38,10 @@ interface RepositoryModule {
     fun bindStudyRepository(
         repositoryImpl: StudyRepositoryImpl
     ): StudyRepository
+
+    @Binds
+    fun bindUserRepository(
+        repositoryImpl: UserRepositoryImpl
+    ): UserRepository
 
 }
