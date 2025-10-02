c68a5fb2ee05285f169ab97d2483660105a56e53
Gyuri <ygreee0320@naver.com>
2025-10-02 18:37:49 +0900

    #90 chore: target SDK version 34 -> 35

--- build-logic/convention/src/main/java/com/ds/studify/convention/ProjectConfiguration.kt
+++ build-logic/convention/src/main/java/com/ds/studify/convention/ProjectConfiguration.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object ProjectConfiguration {
-    const val COMPILE_SDK = 34
-    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 35
+    const val TARGET_SDK = 35
     const val MIN_SDK = 27
     val SOURCE_COMPATIBILITY = JavaVersion.VERSION_17
     val TARGET_COMPATIBILITY = JavaVersion.VERSION_17
